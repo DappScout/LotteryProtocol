8d2115710dab9060a4b143cf2268ad570db5fd8c
DappScout <151783447+DappScout@users.noreply.github.com>
2025-07-18 19:26:44 +0200

    add test sample

---
+++ test/Lottery.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.30;
+
+import "forge-std/Test.sol";
+
+contract LotteryTest is Test {
+
+function testSample() public {
+    assert(true);
+    }
+}
